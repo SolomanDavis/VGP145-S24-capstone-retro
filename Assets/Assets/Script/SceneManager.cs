f755f218f02930e534ed2c69d42030ee1c4c7edb
Daniel Baker <daniel.baker@torontofilmschool.ca>
2024-04-23 16:30:29 -0400

    Dan-Mark-SceneManager-Update
    Generated SceneManager script and added Canvas to Game Scene, and added Canvas to new Title Scene. Quit buttons working, and Resume button working from Pause Menu in Game Scene.
    Need GameManager pushed to Master to get other buttons working.

---
+++ Assets/Assets/Script/SceneManager.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    [Header("Button")]
+    public Button startButton;
+    public Button mainMenuQuitButton;
+    public Button resumeButton;
+    public Button playAgainButton;
+    public Button mainMenuButton;
+    public Button gameOverQuitButton;
+    public Button pauseQuitButton;
+
+    [Header("Menus")]
+    public GameObject mainMenu;
+    public GameObject pauseMenu;
+    public GameObject gameOverMenu;
+
+    [Header("Text")]
+    public TMP_Text scoreText;
+    public TMP_Text livesText;
+
+    static SceneManager instance = null;
+    public static SceneManager Instance => instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        if (mainMenuQuitButton)
+            mainMenuQuitButton.onClick.AddListener(Quit);
+        if (gameOverQuitButton)
+            gameOverQuitButton.onClick.AddListener(Quit);
+        if (pauseQuitButton)
+            pauseQuitButton.onClick.AddListener(Quit);
+
+        if (resumeButton)
+            resumeButton.onClick.AddListener(() => SetMenus(null, pauseMenu));
+    }
+
+
+
+    void SetMenus(GameObject menuToActive, GameObject menuToInactive)
+    {
+        if (menuToActive)
+            menuToActive.SetActive(true);
+
+        if (menuToInactive)
+            menuToInactive.SetActive(false);
+    }
+
+
+
+
+    private void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pauseMenu) return;
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+
+        }
+        if (pauseMenu.activeInHierarchy)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+
+
+
+
+}
