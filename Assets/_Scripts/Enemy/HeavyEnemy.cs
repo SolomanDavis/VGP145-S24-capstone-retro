6f9a2a5f891097bf3104ae72d8be0d554dbd363a
Estelle Schoeman <Estelle.Schoeman@torontofilmschool.ca>
2024-05-08 19:21:28 -0400

    Heavy Enemy Skeleton

---
+++ Assets/_Scripts/Enemy/HeavyEnemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeavyEnemy : Enemy
+
+    // private int _lives
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /* bool isFacingDown()
+        {
+        using raccasting
+        if true shoot()
+        use player location
+        } 
+        */
+
+        /* onCollision with playerProjectile()
+        {
+           call damageFunction()
+        } */
+
+
+    }
+
+    //  void heaveyEnemyDamage()
+    //{}
+
+    // void changeColor()
+    //{  lives -1 }
+
+    // void heaveyDeath()
+    // call EnemyDeath () when lives == 0
+}
+
