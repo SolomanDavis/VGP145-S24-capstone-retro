bc93504d3201c52e1019f31740914c07001ac861
Daniel Baker <daniel.baker@torontofilmschool.ca>
2024-05-07 20:03:58 -0400

    Dan Mark Enemy and Projectile Script
    Updated Enemy script with the suggestions from Ziyad.
    Updated Shoot function, perameters Speed will equal speed of projectile and Offset will determine how close to the player the enemy shoots, based on players position when projectile is spawned. Added enemy death method to be called by enemy animation event.
    Added Projectile script. Once PlayerController is added, we will add adjust the velocity of the projectile. Has OnCollision when player is hit to start IsDead animation on Player

---
+++ Assets/_Scripts/Enemy/Enemy.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    protected SpriteRenderer sr;
+    protected Rigidbody2D rb;
+    protected BoxCollider2D bc;
+    protected Animator anim;
+    
+    [SerializeField] private int EnemyHealth;
+    [SerializeField] private EnemyProjectile enemyProjectile;
+    public Transform enemyProjectileSpawn;
+    [SerializeField] private int projectileSpeed;
+    public float TimeToDestroy = 1;
+
+
+    protected virtual void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+
+        EnemyHealth = 2;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    // TriggerOnAnimationEvent
+    public void Shoot(int min, int max)
+    {
+        //This offset will allow the enemy script to choose to fire the projectile
+        //at the player with an offset to the left and right (we think....)
+        int RandomNumberOffset = Random.Range(min, max);
+
+        EnemyProjectile currentProjectile = Instantiate(enemyProjectile, enemyProjectileSpawn.position, enemyProjectileSpawn.rotation);
+        
+        currentProjectile.speed = projectileSpeed;
+
+        currentProjectile.offset = RandomNumberOffset;
+    }
+
+
+
+    public virtual void TakeDamage(int damage)
+    {
+        EnemyHealth -= damage;
+        if (EnemyHealth <= 0)
+        {
+            anim.SetTrigger("IsDead");
+        }
+    }
+
+
+
+        // This should called right at the end of the animation event on enemy death
+    public virtual void EnemyDeath(int score)
+    {
+        GameManager.Instance.AddToScore(score);
+        Destroy(gameObject);
+        // EnemiesOnScreen --;
+        // TotalNumberOfEnemiesKilled ++;
+    }
+
+
+
+    //public void Pathfinding()
+    //{
+
+    //}
+
+}
