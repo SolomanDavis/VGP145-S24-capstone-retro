38fef7178240ebc51289d42f2f47254bff103a18
Estelle Schoeman <Estelle.Schoeman@torontofilmschool.ca>
2024-05-13 23:57:47 -0400

    Add Project direction towards player

--- Assets/_Scripts/Enemy/EnemyProjectile.cs
+++ Assets/_Scripts/Enemy/EnemyProjectile.cs
@@ -13,7 +13,8 @@ public class EnemyProjectile : MonoBehaviour
     [SerializeField] public int lifeTime;
     [SerializeField] private float bulletSpeed = 10f;
 
-    Vector3 lastPos;
+    // TEST: TO MOVE PROJECTILE TOWARD PLAYER
+    private Vector3 _lastPos; 
     // Once PlayerController is pushed, uncomment this out ---------------------------
     //[SerializeField] PlayerController player;
 
@@ -24,7 +25,9 @@ public class EnemyProjectile : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         PlayerController player = FindObjectOfType<PlayerController>();
-        lastPos = new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z);
+        // TEST: TO MOVE PROJECTILE TOWARD PLAYER
+
+        _lastPos = new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z);
 
         // Once PlayerController is pushed, uncomment this out --------------------------
         rb.velocity = -Vector2.up * bulletSpeed;
@@ -35,7 +38,8 @@ public class EnemyProjectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, lastPos, speed * Time.deltaTime);
+        // TEST: TO MOVE PROJECTILE TOWARD PLAYER
+        transform.position = Vector3.MoveTowards(transform.position, _lastPos, speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
