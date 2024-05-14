a97d964f18328fe91a70ec58cec0a0ff54f505c2
JoshuaCTFS <JoshuaAaron.Clarke@torontofilmschool.ca>
2024-05-14 02:28:07 -0400

    Light Enemy Script and Medium Enemy Script Updated
    Commented out strafing so as to preserve for potential future use.
    Also changed parameters on LightEnemy and MediumEnemy to EnemyHP, and edited the corresponding scripts.

--- Assets/_Scripts/Enemy/MediumEnemy.cs
+++ Assets/_Scripts/Enemy/MediumEnemy.cs
@@ -4,45 +4,40 @@ using UnityEngine;
 public class MediumEnemy : Enemy
 {
 
-    public float fireRate = 1f; // Rate of fire (bullets per second)
-    public float strafeDistance = 5.0f;
-    public float strafeDuration = 10f;
+    //public float fireRate = 1f; // Rate of fire (bullets per second)
+    //public float strafeDistance = 5.0f;
+    //public float strafeDuration = 10f;
     public float currentHealth = 2f;
     public int damageTaken;
     public float moveSpeed;
 
     private float nextFireTime; // Time of the next fire
     private bool movingRight = true; //Moving to the right
-    private float strafeTimer = 10f;
+    //private float strafeTimer = 10f;
 
     protected override void Start()
     {
         base.Start();
         damageTaken = 0;
-        StartCoroutine(Countdown());
+        //StartCoroutine(Countdown());
     }
 
-    IEnumerator Countdown() //Strafe Timer for Enemies
-    {
-        while (strafeTimer > 0f)
-        {
-            yield return new WaitForSeconds(1f);
-            strafeTimer--;
-            Debug.Log("Time remaining until direction change:" + strafeTimer);
-        }
-    }
+    //IEnumerator Countdown() //Strafe Timer for Enemies
+        
+        //while (strafeTimer > 0f)
+            //yield return new WaitForSeconds(1f);
+            //strafeTimer--;
+            //Debug.Log("Time remaining until direction change:" + strafeTimer);
 
     //Enemy animations, damage and animations, aim in the direction the enemy is facing, enemy spawning, score
 
     //Placeholder Script for idle animation and change direction
-    private void strafe() //Used to change direction for Strafing
-    {
-        if (strafeTimer <= 0)
-        {
-            moveSpeed *= -1; //will change direction of the unit when timer elapses
-            strafeTimer = 10f;
-        }
-    }
+    //private void strafe() //Used to change direction for Strafing
+
+        //if (strafeTimer <= 0)
+ 
+            //moveSpeed *= -1; //will change direction of the unit when timer elapses
+            //strafeTimer = 10f;
 
     void OnTriggerEnter(Collider other)
     {
@@ -54,10 +49,10 @@ public class MediumEnemy : Enemy
 
     private void DeathAnimation()
     {
-        if (damageTaken == currentHealth)
+        if (currentHealth == 0)
         {
-            anim.SetInteger("NumberOfHits", damageTaken);
-            EnemyDeath(2);
+            anim.SetInteger("EnemyHealth", damageTaken);
+            EnemyDeath(80);
         }
     }
 }
