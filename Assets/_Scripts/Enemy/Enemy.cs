f0a8b3c279dc6aedc36cd7cc6c904616b22f7524
Iam-B0B <kylepinheiro03@gmail.com>
2024-05-19 16:01:08 -0400

    Enemy projectile fire rate
    fire rate set to proper rate of fire

--- Assets/_Scripts/Enemy/Enemy.cs
+++ Assets/_Scripts/Enemy/Enemy.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -13,12 +14,14 @@ public abstract class Enemy : MonoBehaviour
     public Transform enemyProjectileSpawn;
     [SerializeField] private int projectileSpeed;
     public float TimeToDestroy = 1;
+    [SerializeField] private float shootCooldown = 1f;
 
     public event UnityAction EnemyKilled;
 
     public float maxAngle = 45f;
 
     private bool _isPaused = false;
+    private bool _canShoot = true;
 
     private void Awake()
     {
@@ -31,7 +34,8 @@ public abstract class Enemy : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         bc = GetComponent<BoxCollider2D>();
-        anim = GetComponent<Animator>();                
+        anim = GetComponent<Animator>(); 
+        
     }
 
     // Update is called once per frame
@@ -40,11 +44,14 @@ public abstract class Enemy : MonoBehaviour
         if (_isPaused)
             return;
 
-        if (IsLookingDown())
+        if (IsLookingDown() && _canShoot)
         {
-            Shoot();
+           Shoot();
+            
         }
+        
     }
+    
 
     // TriggerOnAnimationEvent
     public void Shoot()
@@ -58,6 +65,10 @@ public abstract class Enemy : MonoBehaviour
         currentProjectile.speed = projectileSpeed;
 
         currentProjectile.offset = 0;
+
+        _canShoot = false;
+
+        StartCoroutine(ShootCooldown());
     }
 
     /* public void Shoot(int min, int max)
@@ -92,17 +103,28 @@ public abstract class Enemy : MonoBehaviour
     
     public bool IsLookingDown()
     {
-        Vector3 upVector = transform.position - Vector3.up;
+        Vector3 downVector = Vector3.down;
+        //Vector3 upVector = transform.position - Vector3.up;
         //upVector.Normalize();
 
-        Debug.DrawLine(transform.position, upVector, Color.red);
+        //Debug.DrawLine(transform.position, upVector, Color.red);
+        Debug.DrawLine(transform.position, transform.position + transform.up, Color.red);
 
         Debug.DrawLine(Vector3.zero, Vector3.up, Color.green);
 
-        float angle = Vector3.Angle(transform.up, upVector);
+        float angle = Vector3.Angle(transform.up, downVector);
+
+        //float angle = Vector3.Angle(transform.up, upVector);
+
         //Debug.Log("Angle: " + angle);
 
         return angle <= maxAngle;
+    }
+
+    private IEnumerator ShootCooldown()
+    {
+        yield return new WaitForSeconds(shootCooldown);
+        _canShoot = true;
     }
 
 }
